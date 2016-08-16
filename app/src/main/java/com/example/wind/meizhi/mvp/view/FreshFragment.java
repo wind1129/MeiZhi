04dc19caca2dab6941c533d594a41cda39285dc1
Summers <wind1218@sina.cn>
2016-08-16 18:33:33 +0800

    新的更改

--- app/src/main/java/com/example/wind/meizhi/mvp/view/FreshFragment.java
+++ app/src/main/java/com/example/wind/meizhi/mvp/view/FreshFragment.java
@@ -4,4 +4,8 @@ package com.example.wind.meizhi.mvp.view;
  * Created by Summers on 2016/8/15.
  */
 public abstract class FreshFragment extends RecyclerFragment {
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
 }
